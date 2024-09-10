c01316336c3065a41fc01dc1f019e83647eeda3b
Giovanna Gabriela <131730005+GiovannaGabriela@users.noreply.github.com>
2024-09-10 10:23:06 -0300

    Update Book.java

--- Atividade_AOS/demo/src/main/java/com/atividade/aos/demo/model/Book.java
+++ Atividade_AOS/demo/src/main/java/com/atividade/aos/demo/model/Book.java
@@ -1,5 +1,19 @@
 package com.atividade.aos.demo.model;
 
+import jakarta.persistence.*;
+import java.util.List;
+
+@Entity
 public class Book {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String isbn;//Número Padrão Internacional de Livro, basicamente o rg do livro, ex: 9780645570625 é o isbn de "shadow of the conqueror"
+
+    @ManyToOne
+    @JoinColumn(name = "author_id")
+    private Author author;
+
 }
+
