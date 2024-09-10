5691410708debffd9d1f8f5fe3fd15cf9fc20cb0
Giovanna Gabriela <131730005+GiovannaGabriela@users.noreply.github.com>
2024-09-10 09:32:31 -0300

    Update BookService.java

--- Atividade_AOS/demo/src/main/java/com/atividade/aos/demo/service/BookService.java
+++ Atividade_AOS/demo/src/main/java/com/atividade/aos/demo/service/BookService.java
@@ -1,5 +1,29 @@
 package com.atividade.aos.demo.service;
 
+import com.example.library.model.Book;
+import com.example.library.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+@Service
 public class BookService {
-    
+    private final BookRepository bookRepository;
+    private final RestTemplate restTemplate;
+
+    @Value("${external.api.url}")
+    private String externalApiUrl;
+
+    public BookService(BookRepository bookRepository, RestTemplate restTemplate) {
+        this.bookRepository = bookRepository;
+        this.restTemplate = restTemplate;
+    }
+
+    public String fetchBookInfoFromGoogle(String query) {
+        String url = externalApiUrl + "?q=" + query;
+        return restTemplate.getForObject(url, String.class);
+    }
+
 }
