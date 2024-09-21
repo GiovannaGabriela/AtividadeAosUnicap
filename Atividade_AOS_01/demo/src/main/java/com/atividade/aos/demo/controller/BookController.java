0c845f884ff32dd4a14d518e22bbd83c3ee101ba
Giovanna Gabriela <131730005+GiovannaGabriela@users.noreply.github.com>
2024-09-21 17:48:34 -0300

    Update BookController.java

--- Atividade_AOS_01/demo/src/main/java/com/atividade/aos/demo/controller/BookController.java
+++ Atividade_AOS_01/demo/src/main/java/com/atividade/aos/demo/controller/BookController.java
@@ -1,22 +1,22 @@
 package com.atividade.aos.demo.controller;
 
-
-import org.springframework.web.bind.annotation.*;
-
 import com.atividade.aos.demo.service.BookService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/books")
 public class BookController {
+
     private final BookService bookService;
 
     public BookController(BookService bookService) {
         this.bookService = bookService;
     }
 
-    @GetMapping("/external-search")
-    public String searchBooksExternally(@RequestParam String query) {
+    // Endpoint para buscar informações de livros da API do Google Books
+    @GetMapping("/books/search")
+    public String searchBooks(@RequestParam String query) {
         return bookService.fetchBookInfoFromGoogle(query);
     }
-
 }
