4973aade411936407c184ea59d32f7e854a982dd
Giovanna Gabriela <131730005+GiovannaGabriela@users.noreply.github.com>
2024-09-21 17:52:02 -0300

    Update Author.java

--- Atividade_AOS_01/demo/src/main/java/com/atividade/aos/demo/model/Author.java
+++ Atividade_AOS_01/demo/src/main/java/com/atividade/aos/demo/model/Author.java
@@ -1,31 +1,20 @@
 package com.atividade.aos.demo.model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.List;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-
 @Entity
+@Getter
+@Setter
 public class Author {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
-    
+
     @OneToMany(mappedBy = "author")
     private List<Book> books;
-
-    public Object getName() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getName'");
-    }
-
-    public void setName(Object name2) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setName'");
-    }
-
 }
