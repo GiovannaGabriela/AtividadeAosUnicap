0750cb55c22fc50ab5653e57d9d999b9775383d1
Giovanna Gabriela <131730005+GiovannaGabriela@users.noreply.github.com>
2024-09-21 17:52:59 -0300

    Update Book.java

--- Atividade_AOS_01/demo/src/main/java/com/atividade/aos/demo/model/Book.java
+++ Atividade_AOS_01/demo/src/main/java/com/atividade/aos/demo/model/Book.java
@@ -1,39 +1,20 @@
 package com.atividade.aos.demo.model;
 
 import jakarta.persistence.*;
-import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
-    private String isbn;//Número Padrão Internacional de Livro, basicamente o rg do livro, ex: 9780645570625 é o isbn de "shadow of the conqueror"
+    private String isbn; // Número Padrão Internacional de Livro
 
     @ManyToOne
     @JoinColumn(name = "author_id")
     private Author author;
-
-    public Object getTitle() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getTitle'");
-    }
-
-    public void setTitle(Object title2) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setTitle'");
-    }
-
-    public Object getIsbn() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getIsbn'");
-    }
-
-    public void setIsbn(Object isbn2) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setIsbn'");
-    }
-
 }
-
