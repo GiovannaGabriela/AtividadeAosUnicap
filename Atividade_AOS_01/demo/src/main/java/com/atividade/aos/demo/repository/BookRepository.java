bad83fd201c6b9ed002a4b6f7c665aeeaa8450db
Giovanna Gabriela <131730005+GiovannaGabriela@users.noreply.github.com>
2024-09-21 17:54:42 -0300

    Update BookRepository.java

--- Atividade_AOS_01/demo/src/main/java/com/atividade/aos/demo/repository/BookRepository.java
+++ Atividade_AOS_01/demo/src/main/java/com/atividade/aos/demo/repository/BookRepository.java
@@ -1,9 +1,7 @@
 package com.atividade.aos.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.atividade.aos.demo.model.Book;
-
 import java.util.List;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
