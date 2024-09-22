c315a86a3b9f646a531da2e57ca51e022e95b97a
Giovanna Gabriela <131730005+GiovannaGabriela@users.noreply.github.com>
2024-09-21 23:05:13 -0300

    Delete AtividadeAosUnicap-main/Atividade_AOS_01/demo directory

--- AtividadeAosUnicap-main/Atividade_AOS_01/demo/src/main/java/com/atividade/aos/demo/repository/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package com.atividade.aos.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.atividade.aos.demo.model.Book;
-import java.util.List;
-
-public interface BookRepository extends JpaRepository<Book, Long> {
-    List<Book> findByTitleContaining(String title);
-}
