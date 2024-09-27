e7e9c21f3dd9cfc94f85c2db9a5f5e1810f71505
Giovanna Gabriela <131730005+GiovannaGabriela@users.noreply.github.com>
2024-09-27 09:22:35 -0300

    Delete AtividadeAosUnicap-main/Atividade_AOS_01/demo directory

--- AtividadeAosUnicap-main/Atividade_AOS_01/demo/src/main/java/com/atividade/aos/demo/exception/AuthorNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.atividade.aos.demo.exception;
-
-public class AuthorNotFoundException extends RuntimeException {
-    public AuthorNotFoundException(String message) {
-        super(message);
-    }
-}
